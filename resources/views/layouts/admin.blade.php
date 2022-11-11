718f0c1f24ffab3e13815f11067f1bd229619c32
anantaputra <nanta.pslove@gmail.com>
2022-11-11 17:50:14 +0700

    thirdtnth commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -24,7 +24,7 @@
 
     @include('partials.admin-nav')
 
-    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+    <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg pt-5">
 
     @yield('content')
 
