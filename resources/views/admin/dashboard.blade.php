1380ea67f630fab090a3a326f3122d634d6921d4
anantaputra <nanta.pslove@gmail.com>
2022-11-11 17:45:05 +0700

    thirdtnth commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 
-<div class="fixed-plugin">
     <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
       <i class="material-icons py-2">settings</i>
     </a>
